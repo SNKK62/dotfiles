9968fc1e7773ba766860b799fcc9aca78fd02050
SNKK62 <koki.seno62@gmail.com>
2024-03-02 13:14:54 +0900

    change gitgutter color to upper case

--- nvim/lua/plugins/gitgutter.lua
+++ nvim/lua/plugins/gitgutter.lua
@@ -1,3 +1,3 @@
-vim.cmd([[highlight GitGutterAdd ctermfg=green]])
-vim.cmd([[highlight GitGutterChange ctermfg=blue]])
-vim.cmd([[highlight GitGutterDelete ctermfg=red]])
+vim.cmd([[highlight GitGutterAdd ctermbg=Green ]])
+vim.cmd([[highlight GitGutterChange ctermbg=Blue ]])
+vim.cmd([[highlight GitGutterDelete ctermbg=Red ]])
