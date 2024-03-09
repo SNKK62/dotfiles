424b65eae5e218c5500f7c01c330d3922b4bffd5
SNKK62 <koki.seno62@gmail.com>
2024-03-09 12:56:34 +0900

    add new line to keymap.lua

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -170,5 +170,6 @@ end)
 keymap({ "n", "v" }, "<Leader>ac", "<cmd>Lspsaga code_action<CR>", { silent = true })
 -- float terminal
 keymap({ "n", "t" }, "ƒ", "<cmd>Lspsaga term_toggle<CR>", { silent = true }) -- <A-f>
+
 -- session
 keymap("c", "SS", "SessionSave", { noremap = true })
