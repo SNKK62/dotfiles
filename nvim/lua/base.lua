9a3ec30ba1e375dc289b86c840fac1e0f5a9b408
SNKK62 <koki.seno62@gmail.com>
2024-03-21 00:33:47 +0900

    change quick-scope colorscheme

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -62,4 +62,4 @@ opt.ignorecase = true
 opt.smartcase = true
 
 vim.cmd([[ hi QuickScopePrimary guifg='#fffa99' gui=underline ctermfg=155 cterm=underline ]])
-vim.cmd([[ hi QuickScopeSecondary guifg='#afafff' gui=underline ctermfg=81 cterm=underline ]])
+vim.cmd([[ hi QuickScopeSecondary guifg='#bbffff' gui=underline ctermfg=81 cterm=underline ]])
