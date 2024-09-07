a07bc17da26809d54c2a8fd7ebcedd6f252562ce
SNKK62 <koki.seno62@gmail.com>
2024-09-07 11:11:20 +0900

    concat app.lua with window.lua

--- .hammerspoon/window.lua
+++ .hammerspoon/window.lua
@@ -1,23 +1,65 @@
 local window = require("hs.window")
 local fnutils = require("hs.fnutils")
 local hotkey = require("hs.hotkey")
+local application = require("hs.application")
 
 local utils = require("utils")
 
+local open_wezterm = function()
+	local appName = "WezTerm"
+	local app = application.get(appName)
+
+	if app == nil or app:isHidden() or not (app:isFrontmost()) then
+		application.launchOrFocus(appName)
+	else
+		app:hide()
+	end
+end
+hotkey.bind({ "cmd" }, "g", open_wezterm)
+--
+local open_chrome = function()
+	local appName = "Google Chrome"
+	local app = application.get(appName)
+
+	if app == nil or app:isHidden() or not (app:isFrontmost()) then
+		application.launchOrFocus(appName)
+	else
+		-- app:hide()
+		--
+		app.windows[2]:focus()
+	end
+end
+-- hotkey.bind({ "cmd", "alt" }, "g", open_chrome)
+
+local cycleApp = function(appName)
+	return function()
+		local focusedWin = window.focusedWindow()
+		local app = application.get(appName)
+		local allWins =
+			fnutils.filter(app:allWindows(), fnutils.partial(utils.isInScreenAndHasTitle, focusedWin:screen()))
+		if focusedWin == nil or #allWins == 0 then
+			application.launchOrFocus(appName)
+			return
+		end
+		allWins[#allWins]:focus()
+	end
+end
+hotkey.bind({ "cmd", "alt" }, "g", cycleApp("Google Chrome"))
+-- hotkey.bind({ "cmd", "alt" }, "h", cycleApp("Finder"))
+--
 local function focusNextApp()
 	local currentApp = window.focusedWindow()
 	local sortedWindows =
 		fnutils.filter(window.orderedWindows(), fnutils.partial(utils.isInScreenAndHasTitle, currentApp:screen()))
 	sortedWindows[#sortedWindows]:focus()
 end
-
 hotkey.bind({ "cmd", "ctrl" }, "n", focusNextApp)
 
-hotkey.bind({ "alt", "ctrl", "cmd" }, "Right", function()
+hotkey.bind({ "cmd", "ctrl", "shift" }, "Right", function()
 	local win = window.focusedWindow()
 	win:moveOneScreenEast(false, true, 0.5)
 end)
-hotkey.bind({ "alt", "ctrl", "cmd" }, "Left", function()
+hotkey.bind({ "cmd", "ctrl", "shift" }, "Left", function()
 	local win = window.focusedWindow()
 	win:moveOneScreenWest(false, true, 0.5)
 end)
