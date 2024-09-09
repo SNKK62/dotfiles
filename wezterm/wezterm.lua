3a60ad028be78b635c25fbee4e06356b0a55c41d
SNKK62 <koki.seno62@gmail.com>
2024-09-10 01:02:59 +0900

    upgrade wezterm key bindings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,4 +14,140 @@ config.font = wezterm.font("HackGen35 Console NF", { weight = "Regular", stretch
 config.font_size = 16
 config.audible_bell = "Disabled"
 
+local act = wezterm.action
+config.keys = {
+	-- -- move back one word
+	-- {
+	-- 	key = "",
+	-- 	mods = "",
+	-- 	action = wezterm.action_callback(function(window, pane)
+	-- 		window:perform_action(wezterm.action.SendKey({ key = "[", mods = "CTRL" }), pane)
+	-- 		window:perform_action(wezterm.action.SendKey({ key = "b" }), pane)
+	-- 	end),
+	-- },
+	-- -- move forward one word
+	-- {
+	-- 	key = "",
+	-- 	mods = "",
+	-- 	action = wezterm.action_callback(function(window, pane)
+	-- 		window:perform_action(wezterm.action.SendKey({ key = "[", mods = "CTRL" }), pane)
+	-- 		window:perform_action(wezterm.action.SendKey({ key = "f" }), pane)
+	-- 	end),
+	-- },
+	-- delete word
+	{
+		key = "Backspace",
+		mods = "SHIFT",
+		action = act.SendKey({
+			key = "w",
+			mods = "CTRL",
+		}),
+	},
+}
+
+config.key_tables = {
+	copy_mode = {
+		{
+			key = "Enter",
+			mods = "NONE",
+			action = act.CopyMode("MoveToStartOfNextLine"),
+		},
+		{
+			key = "Escape",
+			mods = "NONE",
+			action = act.Multiple({
+				{ CopyMode = "Close" },
+			}),
+		},
+		{
+			key = "c",
+			mods = "CTRL",
+			action = act.Multiple({
+				{ CopyMode = "Close" },
+			}),
+		},
+		{
+			key = "$",
+			mods = "NONE",
+			action = act.CopyMode("MoveToEndOfLineContent"),
+		},
+		{ key = "0", mods = "NONE", action = act.CopyMode("MoveToStartOfLine") },
+		{
+			key = "^",
+			mods = "NONE",
+			action = act.CopyMode("MoveToStartOfLineContent"),
+		},
+		{ key = "b", mods = "NONE", action = act.CopyMode("MoveBackwardWord") },
+		{
+			key = "LeftArrow",
+			mods = "NONE",
+			action = act.CopyMode("MoveBackwardWord"),
+		},
+		{
+			key = "e",
+			mods = "NONE",
+			action = act.CopyMode("MoveForwardWordEnd"),
+		},
+		{
+			key = "f",
+			mods = "NONE",
+			action = act.CopyMode({ JumpForward = { prev_char = false } }),
+		},
+		{ key = "h", mods = "NONE", action = act.CopyMode("MoveLeft") },
+		{ key = "j", mods = "NONE", action = act.CopyMode("MoveDown") },
+		{ key = "k", mods = "NONE", action = act.CopyMode("MoveUp") },
+		{ key = "l", mods = "NONE", action = act.CopyMode("MoveRight") },
+		{
+			key = "q",
+			mods = "NONE",
+			action = act.Multiple({
+				{ CopyMode = "Close" },
+			}),
+		},
+		{
+			key = "t",
+			mods = "NONE",
+			action = act.CopyMode({ JumpForward = { prev_char = true } }),
+		},
+		{
+			key = "u",
+			mods = "CTRL",
+			action = act.CopyMode({ MoveByPage = -0.5 }),
+		},
+		{
+			key = "v",
+			mods = "NONE",
+			action = act.CopyMode({ SetSelectionMode = "Cell" }),
+		},
+		{
+			key = "v",
+			mods = "SHIFT",
+			action = act.CopyMode({ SetSelectionMode = "Line" }),
+		},
+		{
+			key = "v",
+			mods = "CTRL",
+			action = act.CopyMode({ SetSelectionMode = "Block" }),
+		},
+		{ key = "w", mods = "NONE", action = act.CopyMode("MoveForwardWord") },
+		{
+			key = "RightArrow",
+			mods = "NONE",
+			action = act.CopyMode("MoveForwardWord"),
+		},
+		{
+			key = "y",
+			mods = "NONE",
+			action = act.Multiple({
+				act.CopyTo("PrimarySelection"),
+				act.ClearSelection,
+				-- clear the selection mode, but remain in copy mode
+				act.CopyMode("ClearSelectionMode"),
+			}),
+		},
+		{ key = "UpArrow", mods = "NONE", action = act.CopyMode("MoveUp") },
+		{ key = "DownArrow", mods = "NONE", action = act.CopyMode("MoveDown") },
+	},
+}
+
 return config
