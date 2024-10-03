0e3c59db8b6b0f6baf45ecde96bd691781b7818e
SNKK62 <koki.seno62@gmail.com>
2024-10-03 22:48:40 +0900

    update terminal colorscheme to catppucin

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,7 +8,8 @@ end
 
 config.automatically_reload_config = true
 
-config.color_scheme = "Everforest Dark (Gogh)"
+-- config.color_scheme = "Everforest Dark (Gogh)"
+config.color_scheme = "Catppuccin Frappe"
 config.window_background_opacity = 0.96
 config.font = wezterm.font("HackGen35 Console NF", { weight = "Regular", stretch = "Normal", style = "Normal" })
 config.font_size = 16
@@ -85,13 +86,13 @@ config.keys = {
 		action = act.ActivateTabRelative(-1),
 	},
 	{
-		key = "j",
-		mods = "CTRL|SHIFT",
+		key = "Tab",
+		mods = "ALT|SHIFT",
 		action = act.MoveTabRelative(-1),
 	},
 	{
-		key = "k",
-		mods = "CTRL|SHIFT",
+		key = "Tab",
+		mods = "ALT",
 		action = act.MoveTabRelative(1),
 	},
 
