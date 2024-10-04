14b49728f3ac5de79d33c0904453d165eaeabd65
SNKK62 <koki.seno62@gmail.com>
2024-10-05 00:28:30 +0900

    add ignore file pattern in telescope settings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,5 +3,16 @@ local telescope = require("telescope")
 telescope.setup({
 	defaults = {
 		mappings = {},
+		file_ignore_patterns = {
+			"node%_modules/.*",
+			".git/.*",
+			".cache/.*",
+			".DS_Store",
+		},
+	},
+	pickers = {
+		find_files = {
+			hidden = true,
+		},
 	},
 })
