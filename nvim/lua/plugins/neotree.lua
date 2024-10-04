8ceafa88c68e8d137eebd74117c5e0afc435f113
SNKK62 <koki.seno62@gmail.com>
2024-10-04 15:37:58 +0900

    setting neotree background color

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,4 +1,7 @@
 return function()
+	require("utils").set_highlights("neotree", {
+		NeoTreeNormal = { bg = "none" },
+	})
 	require("neo-tree").setup({
 		auto_clean_after_session_restore = true,
 		close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
