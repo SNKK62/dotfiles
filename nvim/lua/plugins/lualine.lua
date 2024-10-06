bfbf1190b1352838616c4552d6e6ead69034213e
SNKK62 <koki.seno62@gmail.com>
2024-10-07 01:43:52 +0900

    fix lualine format

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,7 +2,11 @@ return function()
 	require("lualine").setup({
 		options = { theme = "catppuccin-mocha" },
 		sections = {
-			lualine_c = { "current session: " .. require("auto-session.lib").current_session_name },
+			lualine_c = {
+				function()
+					return "current session: " .. require("auto-session.lib").current_session_name(true)
+				end,
+			},
 		},
 	})
 end
