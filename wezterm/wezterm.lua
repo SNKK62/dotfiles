3116f86b0e4ae2c33156245f893f9b1eae812c1e
SNKK62 <koki.seno62@gmail.com>
2024-10-10 00:01:32 +0900

    make wezterm cool

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,13 +8,74 @@ end
 
 config.automatically_reload_config = true
 
--- config.color_scheme = "Everforest Dark (Gogh)"
 config.color_scheme = "Catppuccin Mocha"
-config.window_background_opacity = 0.96
+local colors = {
+	catppuccin = {
+		mocha = {
+			background = "#1e1e2e",
+			text = "#cdd6f4",
+			selected = "#eba0ac",
+		},
+	},
+}
+
+config.window_background_opacity = 0.92
+config.macos_window_background_blur = 50
+
 config.font = wezterm.font("HackGen35 Console NF", { weight = "Regular", stretch = "Normal", style = "Normal" })
 config.font_size = 16
+config.use_ime = true
 config.audible_bell = "Disabled"
 
+-- hide title bar
+config.window_decorations = "RESIZE"
+-- hide tab bar
+config.show_tabs_in_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
+config.show_close_tab_button_in_tabs = false
+config.use_fancy_tab_bar = true
+
+config.window_background_gradient = {
+	colors = { colors.catppuccin.mocha.background },
+}
+config.window_frame = {
+	inactive_titlebar_bg = "none",
+	active_titlebar_bg = "none",
+}
+config.colors = {
+	tab_bar = {
+		inactive_tab_edge = "none",
+	},
+}
+
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
+---@diagnostic disable-next-line: unused-local, redefined-local
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local background = colors.catppuccin.mocha.background
+	local foreground = colors.catppuccin.mocha.text
+	local edge_background = "none"
+	if tab.is_active then
+		background = colors.catppuccin.mocha.selected
+		foreground = colors.catppuccin.mocha.background
+	end
+	local edge_foreground = background
+	local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+	return {
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_LEFT_ARROW },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_RIGHT_ARROW },
+	}
+end)
+
+-- for underline of bufferline in neovim
 config.underline_thickness = "2.5px"
 
 local act = wezterm.action
@@ -89,13 +150,13 @@ config.keys = {
 	},
 	{
 		key = "Tab",
-		mods = "ALT|SHIFT",
-		action = act.MoveTabRelative(-1),
+		mods = "ALT",
+		action = act.MoveTabRelative(1),
 	},
 	{
 		key = "Tab",
-		mods = "ALT",
-		action = act.MoveTabRelative(1),
+		mods = "ALT|SHIFT",
+		action = act.MoveTabRelative(-1),
 	},
 
 	-- activate copy mode
